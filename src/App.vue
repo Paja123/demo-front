a1e36d3bc2d19295910d7aa39567a247d7432ba3
Pavle Krstić <pavlekrstic010@gmail.com>
2022-06-26 13:32:47 +0200

    navbar
    Dodali smo top navigation bar koji cemo kasnije modifikovati po potrebi.
    
    Co-Authored-By: MrDotMan02 <104990681+MrDotMan02@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,21 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>-->
+      <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper">
+        <router-link to="/" class="brand-logo">Logo</router-link>
+        <ul class="right hide-on-med-and-down">
+          <li><router-link to="/restorani">Pregled restorana</router-link></li>
+          <!-- <li><router-link to="/">Istorija porudzbina</router-link></li> -->
+          <!-- <li><router-link to="/restorani">Korpa</router-link></li> -->
+        </ul>
+      </div>
+    </nav>
+  </div>
   <router-view/>
 </template>
 <script>
  //import Sidebar from '@/components/Sidebar.vue'
 
 </script>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/RestoranView.vue
+++ src/views/RestoranView.vue
@@ -5,13 +5,13 @@
     <h2 v-if="restoran.lokacija">{{restoran.lokacija.adresa}}</h2>
     <h2>Prosecna ocena: {{restoran.prosecnaOcena}}</h2>
     <h2>komentari</h2>
-    <div v-for="komentar in restoran.komentari" >
+    <div v-for="komentar in restoran.komentari" :key="komentar.id" >
         {{komentar.tekst}}
     </div> 
   </div>
   <section class="fcards">
     <div class="row" style="padding-left: 50px; padding-right: 50 px;">
-      <div v-for="artikal in restoran.ponuda" class="col s12 m12 l4" style="padding: 50px 5px;">
+      <div v-for="artikal in restoran.ponuda" :key="artikal.id" class="col s12 m12 l4" style="padding: 50px 5px;">
         <div class="card">
           <div class="card-image waves-effect waves-block waves-light">
           <img class="activator" src="../assets/giros.jpg">
@@ -25,12 +25,31 @@
           <p>{{artikal.opis}}</p>
         </div>
         <div class="card-action">
-            Ovde ono za kolicinu
+            <button class="waves-effect waves-light btn-large">Dodaj u korpu</button>
         </div>
       </div>
     </div>
     </div>
   </section>
+
+  
+  <!-- <ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+      <div class="background">
+        <img src="images/office.jpg">
+      </div>
+      <a href="#user"><img class="circle" src="images/yuna.jpg"></a>
+      <a href="#name"><span class="white-text name">John Doe</span></a>
+      <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
+    </div></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+    <li><a href="#!">Second Link</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Subheader</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+  </ul>
+  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+         -->
 </template>
 <script>
 //import ProductCard from '@/components/ProductCard.vue'
